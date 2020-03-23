2293b957be63f605e28b4fe87ade6b01fdf416e9
Meher Chandan <meherchandan12@gmail.com>
2020-03-23 23:34:17 +0400

    added initial code

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import AddEntry from './components/AddEntry'
+export default function App() {
+  return (
+    <View >
+      <AddEntry/>
+    </View>
+  );
+}
